a792e72ad80fd6f136db5cfec40cca8daffe0b88
gliedabtrennung <d_kinisbek@kbtu.kz>
2023-05-04 01:51:08 +0600

    first

--- tsis10/main.py
+++ tsis10/main.py
@@ -1,90 +1,81 @@
 ﻿import psycopg2
-import re
+import csv
 
-def main():
-    connection = psycopg2.connect(
-        host="localhost",
-        database="postgres",
-        user="postgres",
-        password= "sss13.08"
+conn = psycopg2.connect(
+    host="localhost",
+    database="postgres",
+    user="postgres",
+    password= "sss13.08"
     )
+
+cur = conn.cursor()
+conn.autocommit = True
+
+with conn.cursor() as cursor:
+    cursor.execute("""CREATE TABLE IF NOT EXISTS phonebook(
+    name VARCHAR(20) NOT NULL,
+    phone VARCHAR(20) NOT NULL
+    ) """)
     
-    running = True
-    mode = 'ASC'
-    connection.autocommit = True
-
-    with connection.cursor() as cursor:
-        cursor.execute("""CREATE TABLE IF NOT EXISTS phonebook(
-        first_name VARCHAR(20) NOT NULL,
-        phone_number VARCHAR(20) NOT NULL
-        ) """)
+run = True    
+while run:
+    print(
+        "[0] Quit\n[1] Add Contact\n[2] Update Contact\n[3] Delete Contact\n[4] Print Data\n[5] Add CSV - file")
+    select = int(input())
+    
+    if select == 0:
+        run = False
         
-    while running:
-        selection = int(input("""
-                      Select mode:
-                      1 - Add
-                      2 - Delete
-                      3 - Edit
-                      4 - Look
-                      5 - Clear book
-                      6 - Resort
-                      7 - Import from CSV
-                      """
-                      ))
+    if select == 1:
+        print("Insert name:")
+        name = input()
         
-        if selection == 1:
-            name_to_insert = input("Enter name: ")
-            number_to_insert=input("enter phone number: ")
-            with connection.cursor() as cursor:
-                cursor.execute(f"""INSERT INTO phonebook(first_name,phone_number)
-                VALUES ('{name_to_insert}','{number_to_insert}')""")
-                
-        elif selection == 2:
-            name_to_delete=input("enter to delete: ")
-            with connection.cursor() as cursor:
-                cursor.execute(f"DELETE FROM phonebook WHERE first_name = '{name_to_delete}' OR phone_number = '{name_to_delete}'")                
-                
-        elif selection == 3:
-            editing_name=input("Select the contact to edit: ")
-            new_name=input("New name: ")
-            new_number=input("New phone number: ")
-            with connection.cursor() as cursor:
-                cursor.execute(f"UPDATE phonebook SET first_name='{new_name}' WHERE first_name='{editing_name}'")
-                cursor.execute(f"UPDATE phonebook SET phone_number='{new_number}' WHERE first_name='{new_name}'")                
-                
-        elif selection == 4:
-            with connection.cursor() as cursor:
-                if mode =='ASC':
-                    cursor.execute(f"""SELECT * FROM phonebook ORDER BY first_name ASC""")
-                    
-                if mode =='DESC':
-                    cursor.execute(f"""SELECT * FROM phonebook ORDER BY first_name DESC""")
-                    
-                all=cursor.fetchall()
-                for _,name,phone in all:
-                    print("|"+name+"---"+phone+"|")
+        print("Insert number:")
+        phone = input()
+        
+        cur.execute(f"INSERT INTO phonebook (name, phone) VALUES ('{name}', '{phone}')")
 
-        elif selection == 5:
-            with connection.cursor() as cursor:
-                cursor.execute("TRUNCATE TABLE phonebook;")
+    if select == 2:
+        print("Select the contact to update:")
+        name = input()
+        new_name=input("New name: ")
+        new_number=input("New phone number: ")
+        with conn.cursor() as cursor:
+            cursor.execute(f"UPDATE phonebook SET name='{new_name}' WHERE name='{name}'")
+            cursor.execute(f"UPDATE phonebook SET phone='{new_number}' WHERE name='{new_name}'") 
+        
+    if select == 3:
+        print("Select the contact name to delete:")
+        name = input()
+        
+        print("Select the contact number to delete:")
+        phone = input()
+        
+        cur.execute(f"DELETE FROM phonebook where ('{name}', '{phone}')")
+        
+    if select == 4:
+        print("Select 0 to [A - Z]\nSelect 1 to [Z - A]")
+        sorting = int(input())
+        
+        if sorting == 0:
+            cur.execute("SELECT * FROM phonebook ORDER BY name ASC")
+            data = cur.fetchall()
+            
+        if sorting == 1:
+            cur.execute("SELECT * FROM phonebook ORDER BY name DESC")
+            data = cur.fetchall()
+            
+        for i in data:
+            print(i)
+            
+    if select == 5:
+        with open('table.csv') as file:
+            reader = csv.reader(file, delimiter=',')
+            
+            for i in reader:
+                name = i[0]
+                phone = i[1]
                 
-        elif selection == 6:
-            mode_change=input("1 - [A-Z], 2 - [Z-A]")
-            if mode_change == 1:
-                mode = 'ASC'
-            else:
-                mode = 'DESC'
+                cur.execute(f"INSERT INTO phonebook (name, phone) VALUES ('{name}', '{phone}')")
                 
-        elif selection == 7:
-            with connection.cursor() as cursor:
-                cursor.execute(f"""
-                                COPY phonebook(first_name, phone_number)
-                                FROM 'C:\\Users\\Acer\\Desktop\\pp2\\pp2-22B030135\\tsis10\\phones.csv'
-                                DELIMITER ','
-                                CSV HEADER;
-                               """)
-
-    connection.close()
-
-if __name__ == "__main__":
-    main()
+    conn.commit()
